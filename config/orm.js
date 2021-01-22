08b15d3740a5caa38b01b184659e5c6bc4983b27
Dylan Frank <drfrank22@gmail.com>
2021-01-21 21:25:33 -0500

    creating full stack files

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+const connection = require("./connection");
+
+var orm = {
+
+    selectAll: () {
+
+    };
+
+    insertOne: () {
+
+    };
+
+    updateOne: () {
+
+    };
+    
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+use burgers_db;
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Jalapeno Burger", false),
+    ("Bacon Cheddar Burger", false),
+    ("Bison Burger", true),
+    ("Carolina Reaper Burger", false);
+
