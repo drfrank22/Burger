03a3ceb11e5397b17f782bf4263a42efd8bdb6b3
Dylan Frank <drfrank22@gmail.com>
2021-01-23 00:19:04 -0500

    created orm.js code

--- config/orm.js
+++ config/orm.js
@@ -2,17 +2,32 @@ const connection = require("./connection");
 
 var orm = {
 
-    selectAll: () {
+    selectAll: function(callback) {
         // retrieve data from db
-    };
+        connection.query("SELECT * FROM burgers", function(err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    },
 
-    insertOne: () {
+    insertOne: function(burger_name, callback) {
         // insert data in db
-    };
+        connection.query("INSERT INTO burgers SET ?", {
+            burger_name: burger_name,
+            devoured: false
+        }, function(err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    },
 
-    updateOne: () {
+    updateOne: function(burgerId, callback) {
         // update data in db
-    };
+        connection.query("UPDATE burgers SET ? WHERE ?", [{devoured: true}, {id: burgerId}], function (err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    }
     
 };
 
