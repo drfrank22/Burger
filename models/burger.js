2c312b97202a94c31eee6f2442dbba2eb704baf6
Dylan Frank <drfrank22@gmail.com>
2021-01-23 00:26:06 -0500

    created code for burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,6 +2,24 @@ const orm = require("../config/orm");
 
 var burger = {
 
+    selectAll: function(callback) {
+        orm.selectAll(function(res) {
+            callback(res);
+        });
+    },
+
+    insertOne: function(burger_name, callback) {
+        orm.insertOne(burger_name, function(res) {
+            callback(res);
+        });
+    },
+
+    updateOne: function(burgerId, callback) {
+        orm.updateOne(burgerId, function(res) {
+            callback(res);
+        });
+    }
+
 };
 
 module.exports = burger;
